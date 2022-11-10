1bc7334fe0fed9b1941b9199353723259db8806c
Zach Hensler <zhensler12@gmail.com>
2022-11-09 22:24:39 -0500

    Add Hash Routing

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home:React.FC = () => {
     return (
         <div>
             Home Page
+            <Link to="/" >Home</Link>
+            <Link to="/cart">Cart</Link>
         </div>
     );
 };
